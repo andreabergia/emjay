62779a74ecc0bbafe1531e2466b1cc48f9bbce37
Andrea Bergia <andreabergia@gmail.com>
2024-03-08 14:58:37 +0100

    Extended lexer
    Added basic keywords and operators
    Also supported some basic floating numbers. Missing scientific notation though.

--- src/lexer.rs
+++ src/lexer.rs
@@ -10,7 +10,7 @@ enum LexingError {
 }
 
 impl From<ParseFloatError> for LexingError {
-    fn from(err: ParseFloatError) -> Self {
+    fn from(_: ParseFloatError) -> Self {
         Self::InvalidFloat
     }
 }
@@ -19,14 +19,64 @@ impl From<ParseFloatError> for LexingError {
 #[logos(skip r"[ \t]+")]
 #[logos(error = LexingError)]
 enum Token<'source> {
-    #[regex(r"0|[1-9][0-9]*", |lex| lex.slice().parse())]
+    #[regex(r"not")]
+    Not,
+    #[regex(r"and")]
+    And,
+    #[regex(r"or")]
+    Or,
+    #[regex(r"xor")]
+    Xor,
+    #[regex(r"if")]
+    If,
+    #[regex(r"else")]
+    Else,
+
+    #[regex(r"\+")]
+    Plus,
+    #[regex(r"-")]
+    Minus,
+    #[regex(r"\*")]
+    Multiply,
+    #[regex(r"/")]
+    Divide,
+
+    #[regex(r"\.")]
+    Dot,
+    #[regex(r"=")]
+    Equal,
+
+    #[regex(r"==")]
+    DoubleEqual,
+    #[regex(r"!=")]
+    Different,
+    #[regex(r"<")]
+    Lesser,
+    #[regex(r"<=")]
+    LesserOrEqual,
+    #[regex(r">")]
+    Greater,
+    #[regex(r">=")]
+    GreaterOrEqual,
+
+    #[regex(r"\(")]
+    OpenParenthesis,
+    #[regex(r"\)")]
+    CloseParenthesis,
+    #[regex(r"\[")]
+    OpenBracket,
+    #[regex(r"\]")]
+    CloseBracket,
+    #[regex(r"\{")]
+    OpenBrace,
+    #[regex(r"\}")]
+    CloseBrace,
+
+    #[regex(r"(0|[1-9][0-9]*)(\.[0-9]*)?", |lex| lex.slice().parse())]
     FloatNumber(f64),
 
     #[regex(r"[a-zA-Z][a-zA-Z0-9_]*")]
     Identifier(&'source str),
-
-    #[regex(r"\+")]
-    PlusOperator,
 }
 
 #[cfg(test)]
@@ -44,18 +94,73 @@ mod tests {
     }
 
     #[test]
+    fn lex_keywords() {
+        check_lex_one_token("not", Token::Not);
+        check_lex_one_token("and", Token::And);
+        check_lex_one_token("or", Token::Or);
+        check_lex_one_token("xor", Token::Xor);
+        check_lex_one_token("if", Token::If);
+        check_lex_one_token("else", Token::Else);
+    }
+
+    #[test]
+    fn lex_operators() {
+        check_lex_one_token("+", Token::Plus);
+        check_lex_one_token("-", Token::Minus);
+        check_lex_one_token("*", Token::Multiply);
+        check_lex_one_token("/", Token::Divide);
+
+        check_lex_one_token(".", Token::Dot);
+        check_lex_one_token("=", Token::Equal);
+
+        check_lex_one_token("==", Token::DoubleEqual);
+        check_lex_one_token("!=", Token::Different);
+        check_lex_one_token("<", Token::Lesser);
+        check_lex_one_token("<=", Token::LesserOrEqual);
+        check_lex_one_token(">", Token::Greater);
+        check_lex_one_token(">=", Token::GreaterOrEqual);
+
+        check_lex_one_token("(", Token::OpenParenthesis);
+        check_lex_one_token(")", Token::CloseParenthesis);
+        check_lex_one_token("[", Token::OpenBracket);
+        check_lex_one_token("]", Token::CloseBracket);
+        check_lex_one_token("{", Token::OpenBrace);
+        check_lex_one_token("}", Token::CloseBrace);
+    }
+
+    #[test]
     fn lex_numbers() {
+        check_lex_one_token("0", Token::FloatNumber(0.0));
         check_lex_one_token("42", Token::FloatNumber(42.0));
+        check_lex_one_token("0.1", Token::FloatNumber(0.1));
+        check_lex_one_token("1.", Token::FloatNumber(1.0));
+        check_lex_one_token("123.456", Token::FloatNumber(123.456));
     }
 
     #[test]
     fn lex_identifier() {
         check_lex_one_token("alpha", Token::Identifier("alpha"));
+        check_lex_one_token("a_name", Token::Identifier("a_name"));
+        check_lex_one_token("aName123", Token::Identifier("aName123"));
     }
 
     #[test]
-    fn lex_plus_operator() {
-        check_lex_one_token("+", Token::PlusOperator);
+    fn lex_token_sequence() {
+        let mut lex = Token::lexer("3 + alpha");
+
+        assert_eq!(lex.next(), Some(Ok(Token::FloatNumber(3.0))));
+        assert_eq!(lex.slice(), "3");
+        assert_eq!(lex.span(), 0..1);
+
+        assert_eq!(lex.next(), Some(Ok(Token::Plus)));
+        assert_eq!(lex.slice(), "+");
+        assert_eq!(lex.span(), 2..3);
+
+        assert_eq!(lex.next(), Some(Ok(Token::Identifier("alpha"))));
+        assert_eq!(lex.slice(), "alpha");
+        assert_eq!(lex.span(), 4..9);
+
+        assert_eq!(lex.next(), None);
     }
 
     #[test]
