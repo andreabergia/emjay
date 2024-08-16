09b04fc41639a21b4eccddb6f8b24b0439e00973
Andrea Bergia <andreabergia@gmail.com>
2024-08-16 14:32:00 +0200

    wip: pest

--- src/parser.rs
+++ src/parser.rs
@@ -1,99 +1,28 @@
-use crate::lexer::Token;
-use logos::{Lexer, Logos};
+use pest_derive::Parser;
 
-#[derive(Default, Debug)]
-struct ProgramNode {
-    statements: Vec<StatementNode>,
-}
+#[derive(Parser)]
+#[grammar = "grammar.pest"]
+pub struct EmjayParser;
 
-#[derive(Debug)]
-enum StatementNode {
-    Let {
-        identifier: String,
-        expression: ExpressionNode,
-    },
-    Assignment {
-        identifier: String,
-        expression: ExpressionNode,
-    },
-}
+#[cfg(test)]
+mod tests {
+    use super::{EmjayParser, Rule};
+    use pest::Parser;
 
-#[derive(Debug)]
-struct ExpressionNode {
-    term: TermNode,
-    rest: Option<Box<ExpressionNodeRest>>,
-}
-
-#[derive(Debug)]
-struct ExpressionNodeRest {
-    operator: ExpressionNodeRestOperator,
-    expression: ExpressionNode,
-}
-
-#[derive(Debug)]
-enum ExpressionNodeRestOperator {
-    Plus,
-    Minus,
-}
-
-#[derive(Debug)]
-struct TermNode {
-    factor: FactorNode,
-    rest: Option<Box<TermNodeRest>>,
-}
-
-#[derive(Debug)]
-struct TermNodeRest {
-    operator: TermNodeRestOperator,
-    term: TermNode,
-}
-
-#[derive(Debug)]
-enum TermNodeRestOperator {
-    Multiply,
-    Divide,
-}
-
-#[derive(Debug)]
-enum FactorNode {
-    Identifier(String),
-    NumericLiteral(f64),
-}
-
-#[derive(Debug, Clone, PartialEq)]
-pub enum ParsingError {
-    SomeError(String),
-}
-
-fn parse_statement(lexer: &mut Lexer<'_, Token>) -> Result<Option<StatementNode>, ParsingError> {
-
-    // if let Some(token) = lexer.next() {
-    //      match token {
-    //          Ok(Token::Identifier(id)) => Ok(StatementNode::Assignment {
-    //              identifier: (),
-    //              expression: (),
-    //          }),
-
-    //          _ => {
-    //              return Err(ParsingError::SomeError(format!(
-    //                  "unexpected token {:?} at position {:?}",
-    //                  lexer.span(),
-    //                  lexer.span()
-    //              )))
-    //          }
-    //      }
-    //  }
-}
-
-pub fn parse(source: &str) -> Result<ProgramNode, ParsingError> {
-    let mut lexer = Token::lexer(source);
-    let mut program = ProgramNode::default();
-
-    loop {
-        while let Some(statement) = parse_statement(&mut lexer)? {
-            program.statements.push(statement)
+    #[test]
+    fn can_parse_let() {
+        let parsed = EmjayParser::parse(Rule::letStatement, "let x1 = y")
+            .unwrap()
+            .next()
+            .unwrap();
+        match parsed.as_rule() {
+            Rule::letStatement => {
+                let mut inner = parsed.into_inner();
+                let id = inner.next().unwrap().as_str();
+                let expression = inner.next().unwrap().as_str();
+                println!("id: {}, expr: {}", id, expression);
+            }
+            _ => assert!(false),
         }
     }
-
-    Ok(program)
 }
