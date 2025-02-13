a2ffafa32275f3a55ff62aac55073a9ad2c93614
Andrea Bergia <andreabergia@gmail.com>
2025-02-13 17:52:59 +0100

    Added license

--- src/main.rs
+++ src/main.rs
@@ -25,17 +25,13 @@ fn main() {
         .expect("setting default logging subscriber failed");
 
     let source = r"
-        fn main() {
-            return 1000 + f(3, 2, 1);
+
+
+        fn main(x) {
+  let a = 2;
+  return a + 2 + x;
         }
 
-        fn f(x, y, z) {
-            return x * 100 + y * 10 + (g(z) + z) * 2;
-        }
-
-        fn g(z) {
-            return z + 1;
-        }
     ";
 
     let jit_program = jit_compile_program(source, "main").expect("program should compile");
